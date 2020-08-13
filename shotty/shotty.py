0355e7aa43887ff1e513f01cbfa0de814baa528f
maheedhar1998 <maheedhar1998@hotmail.com>
2020-08-13 14:36:03 -0400

    Update fixed the snapshot creation to account for ec2 states

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -25,12 +25,18 @@ def instances():
 @click.option('--project', default=None, help="Only instances for project (tag Project:<name>)")
 def create_snapshots(project):
   """Create snapshots for EC2 instances"""
-  instances = filter_instances()
+  instances = filter_instances(project)
   for i in instances:
+    print("Stopping {0}...".format(i.id))
     i.stop()
+    i.wait_until_stopped()
     for v in i.volumes.all():
       print("Creating snapshot of {0}".format(v.id))
-      v.create_snapshots(Description="Created by Shotty Script")
+      v.create_snapshot(Description="Created by Shotty Script")
+    print("Restarting {0}...".format(i.id))
+    i.start()
+    i.wait_until_running()
+  print("Done")
   return
 
 @instances.command('list')
